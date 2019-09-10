9b8ec216b79a7a65189771c6ba8b3ebc9ac41519
bh90210 <ktc@protonmail.com>
2019-09-10 17:13:21 +0300

    feat: api refactor

--- server/api/clientCommunication.go
+++ server/api/clientCommunication.go
@@ -1,10 +1,10 @@
-package clientCommunication
+package api
 
 import (
 	"context"
 	"net"
 
-	pb "mockServer/api/protobuf"
+	// pb "mockServer/api/protobuf"
 	e "mockServer/errors"
 
 	"google.golang.org/grpc"
@@ -29,7 +29,7 @@ func StartServer() {
 	// create a gRPC server object
 	s := grpc.NewServer(opts...)
 	// attach service to the server
-	pb.RegisterIntercommServer(s, &intercommService{})
+	RegisterIntercommServer(s, &intercommService{})
 	// start the server
 	err2 := s.Serve(lis)
 	e.Handle(err2)
@@ -37,22 +37,22 @@ func StartServer() {
 
 type intercommService struct{}
 
-func (s *intercommService) Upload(stream pb.Intercomm_UploadServer) error {
+func (s *intercommService) Upload(stream Intercomm_UploadServer) error {
 
 	return nil
 }
 
-func (s *intercommService) Download(in *pb.DownloadRequest, stream pb.Intercomm_DownloadServer) error {
+func (s *intercommService) Download(in *DownloadRequest, stream Intercomm_DownloadServer) error {
 
 	return nil
 }
 
-func (s *intercommService) Delete(cont context.Context, in *pb.DeleteRequest) (*pb.DeleteReply, error) {
-	reply := &pb.DeleteReply{}
+func (s *intercommService) Delete(cont context.Context, in *DeleteRequest) (*DeleteReply, error) {
+	reply := &DeleteReply{}
 	return reply, nil
 }
 
-func (s *intercommService) ListFiles(in *pb.ListFilesRequest, stream pb.Intercomm_ListFilesServer) error {
+func (s *intercommService) ListFiles(in *ListFilesRequest, stream Intercomm_ListFilesServer) error {
 
 	return nil
 }
--- server/pkg/filesHandling.go
+++ server/pkg/filesHandling.go
@@ -1,4 +1,4 @@
-package filesHandling
+package pkg
 
 import (
 	"fmt"
